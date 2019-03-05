d43cf257148b47c984f2b6a93291d21bfaa9e87b
mcdaniel13 <whansrl92@gmail.com>
2019-03-05 11:09:22 +0900

    Create primality.cpp

---
+++ miscellaneous/time_complexity_primality/primality.cpp
@@ -0,0 +1,48 @@
+// author: Moongee Cho
+// date: 03/01/2019
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the primality function below.
+string primality(int n) {
+    if(n == 1)
+        return "Not prime";
+    if(n == 2)
+        return "Prime";
+    if(n % 2 == 0)
+        return "Not prime";
+    
+    
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if(n % i == 0)
+            return "Not prime";
+    }
+    
+    return "Prime";
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+    
+    int p;
+    cin >> p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+    for (int p_itr = 0; p_itr < p; p_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        
+        string result = primality(n);
+        
+        cout << result << "\n";
+        fout << result << "\n";
+    }
+    
+    fout.close();
+    
+    return 0;
+}
